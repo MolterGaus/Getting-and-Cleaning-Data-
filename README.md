255a40001523041125c3035679a61f398ea7699a
MolterGaus <paco_ortega2@hotmail.com>
2016-10-07 02:05:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+##Getting and Cleaning Data
+
+File run_analysis contains a script for analyse data of the accelerometers from the Samsung Galaxy S smartphone.
+
+Requirements: The script requires: - a "UCI HAR Data" folder in the working directory, containing the original folder structure and datasets (as unzipped from source file) in the working directory. - dplyr package 
+
+The script run_analusis do:
+
+*Read all files
+*Reads column labels from features.txt
+*Remove special characters: -,(,) and comma from the variable names
+*Convert Activity labels in y_train.txt and y_test.txt to meaningful names like WALKING, SITING, STANDING, etc
+*Appends Activity labels and Subject numbers (columns) to train and test data
+*Merges train and test data
+*Extracts data with mean and std in the variable names
+*Computes col averages and groups by Subject and Activity (dropping rows without data)
+*Writes a text file (FICHERO.csv) 
+
+En el fichero Codebook se encuentra una referencia a todas las variables que contiene "FICHERO.csv"
+
---
+++ run_analysis.R
@@ -0,0 +1,74 @@
+# https://github.com/pedrompsilva/getting_and_cleaning_data/blob/master/run_analysis.R
+#https://github.com/svkm/Getting-and-Cleaning-Data/blob/master/run_analysis.R
+
+#library required
+
+libray(plyr)
+
+# read of columnames and activity names
+
+colnombres <- read.table("./UCI HAR Dataset/features.txt")
+actividades <- read.table("./UCI HAR Dataset/activity_labels.txt")
+names(actividades) <- c("ID", "ACTIVIDAD")
+
+#Remove special characters
+
+colnombres[,2] <- gsub("[-(),.]", " ", colnombres[,2])
+
+#trim
+
+colnombres[,2] <- gsub("^\\s+|\\s+$", "", colnombres[,2])
+
+# building train set
+
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+
+#colum names of train
+
+names(subject_train)<-"sujeto"
+names(X_train) <- t(colnombres[2])
+names(Y_train) <- "ID_ACTIVIDAD"
+
+#create TRAIN
+
+TRAIN <- cbind(subject_train, Y_train, X_train)
+
+#building test set
+
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
+
+#colum names of test
+
+names(subject_test)<-"sujeto"
+names(X_test) <- t(colnombres[2])
+names(Y_test) <- "ID_ACTIVIDAD"
+
+#create TEST
+
+TEST <- cbind(subject_test, Y_test, X_test)
+
+#build DATA with test and train
+
+DATA <- rbind(TRAIN, TEST) 
+
+#Convert Activity labels in y_train.txt and y_test.txt to meaningful names like
+
+DATA <- merge(DATA,actividades,by.x = "ID_ACTIVIDAD",by.y="ID")
+
+#Extracts data with mean and std in the variable names
+
+FINAL_DATA<- cbind(DATA$sujeto,DATA$ACTIVIDAD,DATA[,grep("mean|std",names(DATA))])
+names(FINAL_DATA)[1:2]<-c("sujeto","ACTIVIDAD")
+
+#Computes col averages and groups by Subject and Activity
+
+FICHERO <- ddply(FINAL_DATA, .(sujeto, ACTIVIDAD), colwise(mean))
+
+#Writes a text file
+
+write.csv(FICHERO, file="FICHERO.csv", row.names=FALSE)
+
